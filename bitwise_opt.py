72d29ef57598994763019225baf590ef6298f8db
sripadhvarma <sripadhvarma@gmail.com>
2025-07-05 10:56:22 +0530

    update

--- bitwise_opt.py
+++ bitwise_opt.py
@@ -1,6 +1,9 @@
+n=int(input("enter no.of choices:"))
 for i in range(1,11):
-    n=int(input("enter value:"))
-    if(n==36):
+    num=int(input("enter value:"))
+    if(num==n):
         print("you guessed it right!!")
+        break
     else:
         print("try again")
+print("Sorry your limit is finished.")
