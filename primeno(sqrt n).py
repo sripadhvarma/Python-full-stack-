00531fd8bf844f53ef7354ace5ed80fca09a615c
sripadhvarma <sripadhvarma@gmail.com>
2025-07-07 15:15:27 +0530

    update

--- primeno(sqrt n).py
+++ primeno(sqrt n).py
@@ -1,4 +1,6 @@
 def printfact(a):
+    if(n<=1):
+        print("neither prime nor composite")
     count=0
     for i in range(1,int(a**0.5)+1):
         if(a%i==0):
@@ -7,7 +9,7 @@ def printfact(a):
                 count+=1
     if count==2:
         print("prime")
-    else:
+    if count>2:
         print("not prime")
 n=int(input())
 printfact(n)
